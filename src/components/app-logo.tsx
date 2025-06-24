0e3c583f7b4114a190a93d98adc953c99667495b
elton junior <elton.bn.jr@gmail.com>
2025-06-24 22:57:16 +0000

    esse o logotipo da empresa, coloque ele no hearte

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -1,28 +1,17 @@
 export const AppLogo = (props: React.SVGProps<SVGSVGElement>) => (
     <svg
-      width="32"
-      height="32"
-      viewBox="0 0 32 32"
-      fill="none"
+      viewBox="0 0 100 100"
       xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
       {...props}
     >
       <path
-        d="M26 16C26 21.5228 21.5228 26 16 26C10.4772 26 6 21.5228 6 16C6 10.4772 10.4772 6 16 6C21.5228 6 26 10.4772 26 16Z"
-        stroke="currentColor"
-        strokeWidth="2.5"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M48.5,95.5 L13,34.5 L19,26.5 L28.5,26.5 L36.5,10.5 L46,10.5 L50,19 L54,10.5 L63.5,10.5 L71.5,26.5 L81,26.5 L87,34.5 L51.5,95.5 L48.5,95.5 Z M78.5,30.5 L73,30.5 L67,18.5 L61,18.5 L56.5,27.5 L50,13.5 L43.5,27.5 L39,18.5 L33,18.5 L27,30.5 L21.5,30.5 L49.5,89.5 L78.5,30.5 Z"
       />
       <path
-        d="M16.5 11L13.5 21"
-        stroke="currentColor"
-        strokeWidth="2.5"
-        strokeLinecap="round"
-      />
-      <path
-        d="M12 17L21 15"
-        stroke="currentColor"
-        strokeWidth="2.5"
-        strokeLinecap="round"
+        d="M45 42 C 45 48, 48 52, 50 54 C 52 52, 55 48, 55 42 C 55 38, 50 35, 50 35 C 50 35, 45 38, 45 42 Z"
       />
     </svg>
   );
