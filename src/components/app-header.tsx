5a09713c4ed6312ee38983309ea0c8ad96cf923b
elton junior <elton.bn.jr@gmail.com>
2025-06-24 23:00:19 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="contai

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -1,15 +1,15 @@
-import { AppLogo } from '@/components/app-logo';
+import Image from 'next/image';
 
 export function AppHeader() {
   return (
     <header className="border-b border-border sticky top-0 bg-black z-10 shadow-sm">
       <div className="container mx-auto flex items-center justify-center h-16 px-4">
-        <div className="flex items-center gap-3 text-white">
-          <AppLogo className="h-7 w-7" />
-          <h1 className="text-xl font-semibold font-headline">
-            Buscador de Leads
-          </h1>
-        </div>
+        <Image
+          src="https://i.imgur.com/psrWNPI.png"
+          alt="Logotipo da Buscador de Leads"
+          width={40}
+          height={40}
+        />
       </div>
     </header>
   );
